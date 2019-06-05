3582428141a60b85b9d26d501f33325641bb0bbd
Vitaly Velikodny <vvelikodny@gmail.com>
2019-06-05 15:23:24 +0300

    fix

--- tests/test_users/factories.py
+++ tests/test_users/factories.py
@@ -5,7 +5,7 @@ from apps.users.models import User
 
 class UserFactory(factory.django.DjangoModelFactory):
     login = factory.Sequence(lambda n: 'User {0}'.format(n))
-    name = factory.Faker('name')
+    email = factory.Faker('email')
     is_staff = False
     is_active = True
 
